0cd2c9e01ad7f327fd9a2cd63e420ce66efe8f48
Ayush Singh <cmsniperline@gmail.com>
2020-04-22 20:29:00 +0530

    Updating configs

--- functions/utility/config.js
+++ functions/utility/config.js
@@ -1,10 +1,10 @@
 module.exports = {
-    apiKey: "AIzaSyDmM9fEBirFeurlNSU28qLdAVV8BKQ76mA",
+    apiKey: "YOUR API KEY",
     authDomain: "cycle-it-be434.firebaseapp.com",
     databaseURL: "https://cycle-it-be434.firebaseio.com",
     projectId: "cycle-it-be434",
     storageBucket: "cycle-it-be434.appspot.com",
-    messagingSenderId: "395259321707",
-    appId: "1:395259321707:web:57a65899baef74eea6c02e",
-    measurementId: "G-JW8BR5ZC7F"
+    messagingSenderId: "YOUR ID",
+    appId: "APP ID",
+    measurementId: "M-ID"
 };
